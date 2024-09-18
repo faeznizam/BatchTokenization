2b2385e06d636fbe085a4479835399aad46a8655
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2024-09-18 16:30:48 +0800

    update code

---
+++ script/helper_for_return_file.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import os
+
+def process_file(file_path):
+    
+    desired_keys = {'merchantReferenceCode',
+                    'decision',
+                    'paySubscriptionCreateReply_subscriptionID',
+                    'paySubscriptionCreateReply_instrumentIdentifierID'
+                    }
+
+    parsed_data = []
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        
+        # Skip the first two rows if necessary
+        if len(lines) > 2:
+            lines = lines[2:]
+
+        for line in lines:
+            line = line.strip()
+            if line:  # Skip empty lines
+                items = line.split(',')
+                parsed_dict = {}
+                for item in items:
+                    if '=' in item:
+                        key, value = item.split('=', 1)  # Split only on the first '='
+                        key = key.strip()
+                        value = value.strip()
+                        if key in desired_keys:
+                            parsed_dict[key] = value
+                parsed_data.append(parsed_dict)
+    
+    # Convert the parsed data to a DataFrame manually if needed
+    parsed_df = pd.DataFrame(parsed_data)
+    
+    # Filter only 'ACCEPT' rows
+    if 'decision' in parsed_df.columns:
+        parsed_df = parsed_df[parsed_df['decision'] == 'ACCEPT']
+    
+    return parsed_df
+
+def map_to_original_file(file_path, parsed_df, folder_path, filename):
+    original_df = pd.read_excel(file_path, dtype={'Post Code' : str, 'Card Number' : str, 
+                                            'Expiry Date': str, 'Payment Submethod': str,
+                                            'Membership No' : str})
+
+    if 'Mobile Phone' in original_df.columns and 'merchantReferenceCode' in parsed_df.columns:
+        merged_df = original_df.merge(
+                                    parsed_df[[
+                                        'merchantReferenceCode',
+                                        'paySubscriptionCreateReply_subscriptionID',
+                                        'paySubscriptionCreateReply_instrumentIdentifierID'
+                                        ]],
+                                    left_on='Mobile Phone', 
+                                    right_on='merchantReferenceCode', 
+                                    how='left')
+        
+        
+        original_df['IPay88 Tokenized ID'] = merged_df['paySubscriptionCreateReply_subscriptionID']
+        original_df['Instrument Id'] = merged_df['paySubscriptionCreateReply_instrumentIdentifierID']
+
+
+    original_df.to_excel(os.path.join(folder_path, f'{filename[:-5]}_SF.xlsx'), index=False)
